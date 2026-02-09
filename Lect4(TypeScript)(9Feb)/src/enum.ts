bada2393196ef7080404ae8fe256d5e5ae5a38a2
sparshsharma81 <81arvindarora@gmail.com>
2026-02-09 14:00:00 +0000

    lecture fse

---
+++ Lect4(TypeScript)(9Feb)/src/enum.ts
@@ -0,0 +1,23 @@
+//what is enum --- enum is set of contant values 
+interface Blog{
+    title:string,  
+    body: string,
+    imageUrl :string
+}
+function addBlog(role:string, blog:Blog):string{
+    if(role == "user"){
+        return "NOT AUTHORIZED"
+    }
+
+    return "BLOG ADDED SUCCESSFULLY"
+   
+    
+}
+addBlog("user",{title: "first blog" , body: "this is my first blog", imageUrl: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"})
+
+//we can use enum to avoid this type of error
+//the error in this code is we can pass any string as role and it will not give us error but we want to restrict the role to only "user" and "admin"
+enum Role{
+    USER = "user",
+    ADMIN = "admin"
+}
